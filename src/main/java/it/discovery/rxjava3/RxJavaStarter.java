03f21f63947cf0143169fc86a4d18bf66793c4a8
Sergiy Morenets <sergey.morenets@gmail.com>
2020-05-09 15:58:50 +0300

    Task #4

---
+++ src/main/java/it/discovery/rxjava3/RxJavaStarter.java
@@ -0,0 +1,23 @@
+package it.discovery.rxjava3;
+
+import io.reactivex.rxjava3.core.Observable;
+
+public class RxJavaStarter {
+    public static void main(String[] args) {
+//        Observable.range('a', 'z' - 'a' + 1)
+//        .map(Character::toChars)
+//        .subscribe(System.out::println);
+//        Observable.generate(() -> 'a',
+//                (v, emitter) -> {
+//                    emitter.onNext(v);
+//                    if(v < 'z') {
+//                        v++;
+//                    } else {
+//                        emitter.onComplete();
+//                    }
+//                    return v;
+//                }).subscribe(System.out::println);
+        Observable.just('i', 't')
+                .subscribe(System.out::println);
+    }
+}
